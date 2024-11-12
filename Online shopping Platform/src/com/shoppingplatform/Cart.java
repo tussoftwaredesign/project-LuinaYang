81cf7740beb63c21cb6f4a9dea1b746a4b227b68
Liuying Yang <liuyingyang2023@gmail.com>
2024-11-12 20:37:05 +0000

    modify Cart to implement method overloading, varargs and LVTI

--- Online shopping Platform/src/com/shoppingplatform/Cart.java
+++ Online shopping Platform/src/com/shoppingplatform/Cart.java
@@ -11,26 +11,46 @@ public class Cart {
         this.products = new ArrayList<>();
     }
 
-    // Add product to cart
+    // Original method to add a single product
     public void addProduct(Product product) {
         products.add(product);
         System.out.println(product.getName() + " added to the cart.");
     }
 
-    // Remove product from cart
-    public void removeProduct(Product product) {
-        products.remove(product);
-        System.out.println(product.getName() + " removed from the cart.");
+    // Overloaded method to add multiple products using varargs
+    public void addProduct(Product... productsToAdd) {
+        for (Product product : productsToAdd) {
+            products.add(product);
+            System.out.println(product.getName() + " added to the cart.");
+        }
     }
 
-    // Calculate total price
+    // Overloaded method to add a list of products
+    public void addProduct(List<Product> productList) {
+        products.addAll(productList);
+        for (Product product : productList) {
+            System.out.println(product.getName() + " added to the cart.");
+        }
+    }
+
+//    // Calculate total price
+//    public double calculateTotal() {
+//        double total = 0;
+//        for (Product product : products) {
+//            total += product.getPrice();
+//        }
+//        return total;
+//    }
+
+    // Local Variable Type Inference(LVTI)
     public double calculateTotal() {
-        double total = 0;
-        for (Product product : products) {
+        var total = 0.0; // Using 'var' for type inference
+        for (var product : products) { // Using 'var' for loop variable
             total += product.getPrice();
         }
         return total;
     }
+
 
     // Display cart contents
     public void displayCart() {
